da4ae9f216701d17970ec01fb17da0d8945e58b6
matsumura-t-mi <matsumura-t-mi@g.bears.ed.jp>
2025-10-27 23:32:13 +0900

    firebase emulatorへの接続設定完了

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
@@ -18,5 +18,15 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 // const analytics = getAnalytics(app);
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('Running in development mode, connecting to Firebase Emulators');
+  try {
+    connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
+    connectFirestoreEmulator(db, '127.0.0.1', 8080);
+  } catch (error) {
+    console.error('Error connecting to Firebase Emulators: ', error);
+  }
+}
 
 export { app, auth, db };
