ee0f8b20124910f5396784c22b45a5a0f0fed672
matsumura-t-mi <matsumura-t-mi@g.bears.ed.jp>
2025-11-04 22:28:33 +0900

    firebase emulatorの設定は諦めて、本番環境で開発を続けることにした。

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -19,29 +19,29 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 // const analytics = getAnalytics(app);
 
-if (process.env.NODE_ENV === 'development') {
-  console.log('Running in development mode, attempting to connect to Firebase Emulators');
+// if (process.env.NODE_ENV === 'development') {
+//   console.log('Running in development mode, attempting to connect to Firebase Emulators');
 
-  try {
-    const authEmulatorHost = process.env.NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST;
-    if (authEmulatorHost) {
-      connectAuthEmulator(auth, `http://${authEmulatorHost}`, { disableWarnings: true });
-      console.log(`Successfully connected to Auth Emulator at ${authEmulatorHost}`);
-    } else {
-      console.log('Auth Emulator host not found, skipping connection.');
-    }
+//   try {
+//     const authEmulatorHost = process.env.NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST;
+//     if (authEmulatorHost) {
+//       connectAuthEmulator(auth, `http://${authEmulatorHost}`, { disableWarnings: true });
+//       console.log(`Successfully connected to Auth Emulator at ${authEmulatorHost}`);
+//     } else {
+//       console.log('Auth Emulator host not found, skipping connection.');
+//     }
 
-    const firestoreEmulatorHost = process.env.NEXT_PUBLIC_FIRESTORE_EMULATOR_HOST;
-    if (firestoreEmulatorHost) {
-      const [host, port] = firestoreEmulatorHost.split(':');
-      connectFirestoreEmulator(db, host, parseInt(port, 10));
-      console.log(`Successfully connected to Firestore Emulator at ${host}:${port}`);
-    } else {
-      console.log('Firestore Emulator host not found, skipping connection.');
-    }
-  } catch (error) {
-    console.error('Error connecting to Firebase Emulators: ', error);
-  }
-}
+//     const firestoreEmulatorHost = process.env.NEXT_PUBLIC_FIRESTORE_EMULATOR_HOST;
+//     if (firestoreEmulatorHost) {
+//       const [host, port] = firestoreEmulatorHost.split(':');
+//       connectFirestoreEmulator(db, host, parseInt(port, 10));
+//       console.log(`Successfully connected to Firestore Emulator at ${host}:${port}`);
+//     } else {
+//       console.log('Firestore Emulator host not found, skipping connection.');
+//     }
+//   } catch (error) {
+//     console.error('Error connecting to Firebase Emulators: ', error);
+//   }
+// }
 
 export { app, auth, db };
